580843356a9c39669e26404683e1a9c08b7191e7
Michael Hutchinson <hutchinson.michael.john@gmail.com>
2022-01-19 11:59:16 +0000

    add colorbar

--- score_sde/utils/vis.py
+++ score_sde/utils/vis.py
@@ -36,16 +36,18 @@ def plot_and_save(
     sphere.draw(ax, color="red", marker=".")
     # sphere.plot_heatmap(ax, pdf, n_points=16000, alpha=0.2, cmap=cmap)
     if x0 is not None:
-        cax = ax.scatter(x0[:,0], x0[:,1], x0[:,2], s=50, color='green')
+        cax = ax.scatter(x0[:, 0], x0[:, 1], x0[:, 2], s=50, color="green")
     if xt is not None:
-        x, y, z = xt[:,0], xt[:,1], xt[:,2]
+        x, y, z = xt[:, 0], xt[:, 1], xt[:, 2]
         c = prob if prob is not None else np.ones([*xt.shape[:-1]])
         cax = ax.scatter(x, y, z, s=50, c=c, cmap=cmap)
     if grad is not None:
         u, v, w = grad[:, 0], grad[:, 1], grad[:, 2]
-        quiver = ax.quiver(x, y, z, u, v, w, length=.2, lw=2, normalize=False, cmap=cmap)
+        quiver = ax.quiver(
+            x, y, z, u, v, w, length=0.2, lw=2, normalize=False, cmap=cmap
+        )
         quiver.set_array(c)
 
+    plt.colorbar()
     plt.savefig(out, dpi=dpi, bbox_inches="tight", transparent=True)
     plt.close(fig)
-
