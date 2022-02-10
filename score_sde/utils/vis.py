a711cc9637e35284273d811a7791ef310e81a415
Michael Hutchinson <hutchinson.michael.john@gmail.com>
2022-02-10 11:25:14 +0000

    vis changes

--- score_sde/utils/vis.py
+++ score_sde/utils/vis.py
@@ -38,7 +38,13 @@ def set_aspect_equal_3d(ax):
     ymean = np.mean(ylim)
     zmean = np.mean(zlim)
 
-    plot_radius = max([abs(lim - mean_) for lims, mean_ in ((xlim, xmean), (ylim, ymean), (zlim, zmean)) for lim in lims])
+    plot_radius = max(
+        [
+            abs(lim - mean_)
+            for lims, mean_ in ((xlim, xmean), (ylim, ymean), (zlim, zmean))
+            for lim in lims
+        ]
+    )
 
     ax.set_xlim3d([xmean - plot_radius, xmean + plot_radius])
     ax.set_ylim3d([ymean - plot_radius, ymean + plot_radius])
@@ -47,9 +53,8 @@ def set_aspect_equal_3d(ax):
     return ax
 
 
-def sphere_plot(ax, color="grey"):
-    # assert manifold.dim == 2
-    radius = 1.
+def get_sphere_coords():
+    radius = 1.0
     # set_aspect_equal_3d(ax)
     n = 200
     u = np.linspace(0, 2 * np.pi, n)
@@ -58,6 +63,13 @@ def sphere_plot(ax, color="grey"):
     x = 1 * np.outer(np.cos(u), np.sin(v))
     y = 1 * np.outer(np.sin(u), np.sin(v))
     z = 1 * np.outer(np.ones(np.size(u)), np.cos(v))
+
+    return x, y, z
+
+
+def sphere_plot(ax, color="grey"):
+    # assert manifold.dim == 2
+    x, y, z = get_sphere_coords()
     ax.plot_surface(x, y, z, rstride=4, cstride=4, color=color, linewidth=0, alpha=0.2)
 
     return ax
@@ -88,24 +100,24 @@ def latlon_from_cartesian(points):
     lat = -jnp.arcsin(z / r)
     lon = jnp.arctan2(y, x)
     lon = jnp.where(lon > 0, lon - math.pi, lon + math.pi)
-    return jnp.concatenate([jnp.expand_dims(lat, -1), jnp.expand_dims(lon, -1)], axis=-1)
+    return jnp.concatenate(
+        [jnp.expand_dims(lat, -1), jnp.expand_dims(lon, -1)], axis=-1
+    )
 
 
-def get_spherical_grid(N, eps=0.):
+def get_spherical_grid(N, eps=0.0):
     lat = jnp.linspace(-90 + eps, 90 - eps, N // 2)
     lon = jnp.linspace(-180 + eps, 180 - eps, N)
     Lat, Lon = jnp.meshgrid(lat, lon)
     latlon_xs = jnp.concatenate([Lat.reshape(-1, 1), Lon.reshape(-1, 1)], axis=-1)
     spherical_xs = (
-            jnp.pi * (latlon_xs / 180.0) + jnp.array([jnp.pi / 2, jnp.pi])[None, :]
-        )
+        jnp.pi * (latlon_xs / 180.0) + jnp.array([jnp.pi / 2, jnp.pi])[None, :]
+    )
     xs = Hypersphere(2).spherical_to_extrinsic(spherical_xs)
     return xs, lat, lon
 
 
-def plot(
-    x0, xt, prob, grad, x0prob=None, size=10, dpi=300, out="out.jpg", color="red"
-):
+def plot(x0, xt, prob, grad, x0prob=None, size=10, dpi=300, out="out.jpg", color="red"):
     fig = plt.figure(figsize=(size, size))
     ax = fig.add_subplot(111, projection="3d")
     ax = remove_background(ax)
@@ -122,7 +134,7 @@ def plot(
     if xt is not None:
         x, y, z = xt[:, 0], xt[:, 1], xt[:, 2]
         c = prob if prob is not None else np.ones([*xt.shape[:-1]])
-        cax = ax.scatter(x, y, z, s=50, vmin=0., vmax=2., c=c, cmap=cmap)
+        cax = ax.scatter(x, y, z, s=50, vmin=0.0, vmax=2.0, c=c, cmap=cmap)
     if grad is not None:
         u, v, w = grad[:, 0], grad[:, 1], grad[:, 2]
         quiver = ax.quiver(
@@ -156,8 +168,8 @@ def scatter_earth(x, ax=None, s=50, color="green"):
 
 
 def earth_plot(cfg, log_prob, train_ds, test_ds, N, azimuth=None):
-    """ generate earth plots with model density or integral paths aka streamplot  """
-    has_cartopy = importlib.find_loader('cartopy')
+    """generate earth plots with model density or integral paths aka streamplot"""
+    has_cartopy = importlib.find_loader("cartopy")
     if not has_cartopy:
         return
 
@@ -168,17 +180,17 @@ def earth_plot(cfg, log_prob, train_ds, test_ds, N, azimuth=None):
     projs = ["ortho", "robinson"]
     # projs = ["ortho"]
 
-    xs, lat, lon = get_spherical_grid(N, eps=0.)
+    xs, lat, lon = get_spherical_grid(N, eps=0.0)
     # ts = [0.01, 0.05, cfg.sde.tf]
     ts = [cfg.sde.tf]
     figs = []
-  
+
     for t in ts:
         print(t)
         fs = log_prob(xs, t).reshape((lat.shape[0], lon.shape[0]), order="F")
         fs = jnp.exp(fs)
         # norm = mcolors.PowerNorm(3.)  # NOTE: tweak that value
-        norm = mcolors.PowerNorm(.2)  # N=500
+        norm = mcolors.PowerNorm(0.2)  # N=500
         fs = np.array(fs)
         # print(np.min(fs).item(), jnp.quantile(fs, np.array([0.1, 0.5, 0.9])), np.max(fs).item())
         fs = norm(fs)
@@ -200,11 +212,13 @@ def earth_plot(cfg, log_prob, train_ds, test_ds, N, azimuth=None):
             # earth features
             ax.add_feature(cfeature.LAND, zorder=0, facecolor="#e0e0e0")
 
-            vmin, vmax = 0., 1.0
+            vmin, vmax = 0.0, 1.0
             # n_levels = 900
             n_levels = 200
             levels = np.linspace(vmin, vmax, n_levels)
-            cmap = sns.cubehelix_palette(light=1.0, dark=0.0, start=0.5, rot=-0.75, reverse=False, as_cmap=True)
+            cmap = sns.cubehelix_palette(
+                light=1.0, dark=0.0, start=0.5, rot=-0.75, reverse=False, as_cmap=True
+            )
             # cmap = sns.cubehelix_palette(as_cmap=True)
             cs = ax.contourf(
                 lon,
@@ -219,12 +233,12 @@ def earth_plot(cfg, log_prob, train_ds, test_ds, N, azimuth=None):
                 cmap=cmap,
                 extend="both",
             )
-        
+
             alpha_gradient = np.linspace(0, 1, len(ax.collections))
             for col, alpha in zip(ax.collections, alpha_gradient):
                 col.set_alpha(alpha)
             # for col in ax.collections[0:1]:
-                # col.set_alpha(0)
+            # col.set_alpha(0)
 
             # add scatter plots of the dataset
             colors = sns.color_palette("hls", 8)
@@ -233,9 +247,23 @@ def earth_plot(cfg, log_prob, train_ds, test_ds, N, azimuth=None):
             test_idx = test_ds.dataset.indices
             samples = train_ds.dataset.dataset.data
             samples = np.array(latlon_from_cartesian(samples)) * 180 / math.pi
-            points = projection.transform_points(ccrs.Geodetic(), samples[:, 1], samples[:, 0])
-            ax.scatter(points[train_idx, 0], points[train_idx, 1], s=0.2, c=[colors[5]], alpha=0.2)
-            ax.scatter(points[test_idx, 0], points[test_idx, 1], s=0.2, c=[colors[0]], alpha=0.2)
+            points = projection.transform_points(
+                ccrs.Geodetic(), samples[:, 1], samples[:, 0]
+            )
+            ax.scatter(
+                points[train_idx, 0],
+                points[train_idx, 1],
+                s=0.2,
+                c=[colors[5]],
+                alpha=0.2,
+            )
+            ax.scatter(
+                points[test_idx, 0],
+                points[test_idx, 1],
+                s=0.2,
+                c=[colors[0]],
+                alpha=0.2,
+            )
             # plt.close(fig)
             figs.append(fig)
 
