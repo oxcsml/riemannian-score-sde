3d5c96c0d1e2f8142134092733855e329461c1a2
Emile Mathieu <emile.mathieu@stats.ox.ac.uk>
2022-05-04 14:51:18 +0100

    update deploy

--- scripts/deploy/sync_keys.sh
+++ scripts/deploy/sync_keys.sh
@@ -4,7 +4,7 @@
 dir_path="`dirname \"$0\"`"
 source $dir_path/config.sh
 
-for NODE_ID in '2' '3' '1' '5'
+for NODE_ID in '1' '2' '3' '5'
 do
-    srun --pty --partition=zizgpu0$NODE_ID-debug ssh-copy-id -i ~/.ssh/$rsa_key zizgpu04.cpu.stats.ox.ac.uk
+    srun --pty --clusters=srf_gpu_01 --partition=zizgpu0$NODE_ID-debug ssh-copy-id -i ~/.ssh/$rsa_key zizgpu04.cpu.stats.ox.ac.uk
 done
--- scripts/deploy/sync_venv.sh
+++ scripts/deploy/sync_venv.sh
@@ -4,7 +4,7 @@
 dir_path="`dirname \"$0\"`"
 source $dir_path/config.sh
 
-for NODE_ID in '2' '3' '1'
+for NODE_ID in '1' '2' '3' '5'
 do
     srun --clusters=srf_gpu_01 --partition=zizgpu0$NODE_ID-debug mkdir -p $parent_dir/$venv &
     srun --clusters=srf_gpu_01 --partition=zizgpu0$NODE_ID-debug rsync -e "ssh -i ~/.ssh/id_rsa_ziz" -a --info=progress2 ziz.stats.ox.ac.uk:$parent_dir/$venv/* $parent_dir/$venv/. &
